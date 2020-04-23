401a5095d68c0309acc64beb27aa5c1c2bf49370
Vladimír Urík <gggedrvideos@gmail.com>
2020-04-23 08:32:57 +0200

    Fix

--- panel/index.php
+++ panel/index.php
@@ -2,7 +2,7 @@
 <head>
     <script src="panel.js"></script>
 </head>
-<body onload="logined(<?php echo $_COOKIE["id"]?>)">
+<body onload="logined(<?php echo $_COOKIE["user_id"]?>)">
 
 </body>
 </html>
