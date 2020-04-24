48950c6e65f933cbba926ed7269cbc836ed49e9e
Vladimír Urík <gggedrvideos@gmail.com>
2020-04-24 18:39:44 +0200

    Biggest WEb optimalization ! Fuck Biggest

--- panel/index.php
+++ panel/index.php
@@ -25,67 +25,10 @@ if($jazyk == null){
         }</script>
 </head>
 <body onload="<?php $id = $_COOKIE["user_id"]; if($_COOKIE["user_id"] == null){ echo "location.href = 'https://discordapp.com/api/oauth2/authorize?client_id=699959562454827009&redirect_uri=https%3A%2F%2Fprivate.greenlandmc.eu%2Ftajny-projekt%2FGGGEDR%2FThumbie%2Fpanel%2Fintegrations%2Fdiscord%2FOAuth2%2Flogin.php&response_type=code&scope=identify%20guilds%20email';";}?>">
-<nav class="navbar navbar-expand-lg bg-transparent">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="https://private.greenlandmc.eu/tajny-projekt/GGGEDR/Thumbie/"><?php include ("src/laungage/". $jazyk .".php"); echo $Home;?><span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about"><?php include ("src/laungage/". $jazyk .".php"); echo $About;?></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#features"><?php include ("src/laungage/". $jazyk .".php"); echo $Features;?></a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <?php include ("src/laungage/". $jazyk .".php"); echo $Doc;?>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="/commands"><?php include ("src/laungage/". $jazyk .".php"); echo $Commands;?></a>
-                    <a class="dropdown-item" href="/tutorials"><?php include ("src/laungage/". $jazyk .".php"); echo $Tutorial;?></a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="/server-settings"><?php include ("src/laungage/". $jazyk .".php"); echo $sett;?></a>
-                </div>
-            </li>
-        </ul>
-        <?php
-        include ("src/laungage/". $jazyk .".php");
-        if($_COOKIE["user_id"] == null){
-            echo "<a class=\"btn btn-outline-secondary my-2 my-sm-0 btn-round btn-sm\" href=\"https://discordapp.com/api/oauth2/authorize?client_id=699959562454827009&redirect_uri=https%3A%2F%2Fprivate.greenlandmc.eu%2Ftajny-projekt%2FGGGEDR%2FThumbie%2Fpanel%2Fintegrations%2Fdiscord%2FOAuth2%2Flogin.php&response_type=code&scope=identify%20guilds%20email\">$Login</a>";
-        } else {
-            $user_id = $_COOKIE["user_id"];
-            $user_avatar = $_COOKIE["user_avatar"];
-            $nick = "<p style='color:orange;'>     <img src='https://cdn.discordapp.com/avatars/$user_id/$user_avatar.webp' height='40' width='40' style='    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    -ms-border-radius: 50%;
-    -o-border-radius: 50%;
-    border-radius: 50%;  '></p>";
-            $combo = $IsLogged . $nick;
-
-            include("src/laungage/" . $jazyk . ".php");
-            echo "<br/>
-<br/>
-<li class=\"nav-item dropdown\" style='text-align: right; vertical-align: top;'>
-                <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\" style='text-align: right; vertical-align: top; color: orange'>
-                    $nick
-                </a>
-                
-                <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\" style='text-align: right; vertical-align: top;'>
-                    <a class=\"dropdown-item\" style='text-align: right; vertical-align: top;' href=\"#\">$Servers</a>
-                    <a class=\"dropdown-item\" style='text-align: right; vertical-align: top;' href=\"#\">$Settings</a>
-                    <div class=\"dropdown-divider\" style='text-align: right; vertical-align: top;'></div>
-                    <a class=\"dropdown-item\" style='text-align: right; vertical-align: top;' onclick='odhlasit()' href=\"https://private.greenlandmc.eu/tajny-projekt/GGGEDR/Thumbie\">$Logout</a>
-                </div>
-            </li>
-            <a>‎‎‎‎‎‎ ‍  ‍  ‍   ‍  ‍  ‍   </a>";
-        }
-        ?>
-    </div>
-</nav>
+<?php
+include ("MicroPaths.php");
+getNavbar($Servers, $Settings, $Logout, $IsLogged, $Login, $Doc, $Commands, $Tutorial, $sett, $About, $Features, $Home);
+?>
 <link rel="stylesheet" type="text/css" href="//wpcc.io/lib/1.0.2/cookieconsent.min.css"/><script src="//wpcc.io/lib/1.0.2/cookieconsent.min.js"></script><script>window.addEventListener("load", function(){window.wpcc.init({"colors":{"popup":{"background":"#23272a","text":"#ffffff","border":"#ffa500"},"button":{"background":"#ffa500","text":"#ffffff"}},"corners":"large","padding":"small","margin":"large","content":{"message":"<?php include ("src/laungage/". $jazyk .".php"); echo $Cookies;?>","link":"<?php include ("src/laungage/". $jazyk .".php"); echo $MoreInfo;?>","button":"<?php include ("src/laungage/". $jazyk .".php"); echo $BTNCookies;?>", "href":"https://www.cookiepolicygenerator.com/live.php?token=4NrLLhbRTNIZB7z1P7PjSqt7uPoA8jge"}})});</script>
 <div class="heading text-center" style="text-align: left !important;">
     <img src="src/img/logo.png" style="text-align: left;" width="75" height="75">
@@ -106,11 +49,6 @@ if($jazyk == null){
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="src/js/now-ui-kit.min.js"></script>
-<div class="footer">
-    <button onclick="setCookie('Laungage', 'en_GB', 7); history.go(0); location.href = '#n';" style="background: transparent; border: transparent"><img src="src/img/en.jpg" width="19" height="9" onclick="setCookie('Laungage', 'en_GB', 7)" data-toggle="tooltip" data-placement="top" title="Click to select this language"></button>
-    <button onclick="setCookie('Laungage', 'cs', 7); history.go(0); location.href = '#n';" style="background: transparent; border: transparent"><img src="src/img/cz.jpg" width="19" height="9" onclick="setCookie('Laungage', 'cs_CZ', 7)" data-toggle="tooltip" data-placement="top" title="Klikni pro vybráni tohohle jazyka"></button>
-    <button onclick="setCookie('Laungage', 'sk', 7); history.go(0); location.href = '#n';" style="background: transparent; border: transparent"><img src="src/img/sk.jpg" width="19" height="9" onclick="setCookie('Laungage', 'sk_SK', 7)" data-toggle="tooltip" data-placement="top" title="Klikni pre vybranie tohto jazyka"></button><br/>
-    <?php include 'src/laungage/en_GB.php'; echo $CreatedBy;?>
-</div>
+<?php getFooter($CreatedBy); ?>
 </body>
 </html>
